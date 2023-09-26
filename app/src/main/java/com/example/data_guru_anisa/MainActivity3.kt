0d0e8541afadbbda71bca536b4408726108b7ac0
AnisaDwiRahmawati123456 <anisadwir61@gmail.com>
2023-09-26 15:24:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/data_guru_anisa/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.data_guru_anisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
